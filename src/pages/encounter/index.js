a414527961d3835078f02eb2a9b74d9b0342ebc0
Vivaldi Ibelio Chandra <vivaldi.ch@gmail.com>
2019-06-27 21:13:34 +0800

    Adds deadly encounter list into the page, random gold loot, and a back button.

--- src/pages/encounter/index.js
+++ src/pages/encounter/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import last from 'lodash/last';
 import capitalize from 'lodash/capitalize';
 
 import Layout from '../../components/layout';
 import CardList from '../../components/card-list';
+import { randomGoldLoot } from '../../utils/random';
 
 const EncounterPage = (props) => {
   const {
@@ -15,9 +17,27 @@ const EncounterPage = (props) => {
 
   return (
     <Layout>
-      <h1>{capitalize(encounterType)} Encounter</h1>
-      <p>Choose one from the following: </p>
-      <CardList type={encounterType} />
+      <div className="flex flex-col-reverse md:flex-row md:justify-between">
+        <h1>{capitalize(encounterType)} Encounter</h1>
+        <div className="text-right pb-3 underline text-blue-500 hover:text-gray-600">
+          <Link to="/">Back</Link>
+        </div>
+      </div>
+      <p className="text-gray-600 text-sm">
+        You gain {randomGoldLoot() * (encounterType === 'deadly' ? 3 : 1)} gp
+      </p>
+      <>
+        <p>Choose one from the following: </p>
+        <CardList type="normal" />
+      </>
+      {
+        encounterType === 'deadly' ? (
+          <div className="my-6">
+            <p>Also choose one from the following: </p>
+            <CardList type="deadly" />
+          </div>
+        ) : ''
+      }
     </Layout>
   );
 };
--- src/utils/random.js
+++ src/utils/random.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-unused-vars */
+
+import times from 'lodash/times';
+
 function getRandomInt(min, max) {
   const intMin = Math.ceil(min);
   const intMax = Math.floor(max);
@@ -15,4 +18,19 @@ export const coinFlip = () => getRandomIntInclusive(0, 1) === 1;
 
 export const randomHitDie = () => 2 * getRandomIntInclusive(3, 6);
 
-export const randomGoldLoot = () => {};
+export const randomGoldLoot = () => getRandomIntInclusive(100, 300);
+
+export const percentileDie = () => getRandomIntInclusive(1, 100);
+
+export const randomEntriesFromArray = (arrLength, noOfEntries = 1) => {
+  const indexArray = times(arrLength, Number);
+
+  for (let i = arrLength - 1; i > 1; i -= 1) {
+    const r = Math.floor(Math.random() * i);
+    const t = indexArray[i];
+    indexArray[i] = indexArray[r];
+    indexArray[r] = t;
+  }
+
+  return indexArray.slice(0, noOfEntries);
+};
