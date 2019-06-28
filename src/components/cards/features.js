89dbe8d86e0095f695242450e5e54870b9004922
Vivaldi Ibelio Chandra <vivaldi.ch@gmail.com>
2019-06-28 13:15:05 +0800

    Features level is now weighted.

--- src/components/cards/features.js
+++ src/components/cards/features.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import reduce from 'lodash/reduce';
+import { percentileDie } from '../../utils/random';
 
 const useFeatures = () => {
   const {
@@ -7,7 +9,7 @@ const useFeatures = () => {
   } = useStaticQuery(
     graphql`
       {
-        allFeaturesJson(filter: { level: { gt: 0, lte: 3 } }) {
+        allFeaturesJson(filter: { level: { lte: 5 } }) {
           nodes {
             level
             name
@@ -24,10 +26,40 @@ const useFeatures = () => {
   return nodes;
 };
 
+const sortFeaturesByLevel = features =>
+  reduce(
+    features,
+    (sorted, f) => {
+      const currentSorted = sorted;
+      if (!currentSorted[f.level]) {
+        currentSorted[f.level] = [];
+      }
+
+      currentSorted[f.level].push(f);
+
+      return currentSorted;
+    },
+    {},
+  );
+
+const getRandomFeaturesByLevel = (features) => {
+  const sortedFeatures = sortFeaturesByLevel(features);
+  const percentileRoll = percentileDie();
+
+  if (percentileRoll <= 40) return sortedFeatures[1];
+  if (percentileRoll <= 69) return sortedFeatures[2];
+  if (percentileRoll <= 94) return sortedFeatures[3];
+  if (percentileRoll <= 99) return sortedFeatures[4];
+  return sortedFeatures[5];
+};
+
 const FeatureCard = () => {
   const features = useFeatures();
+  const sortedFeatures = getRandomFeaturesByLevel(features);
 
-  const randomFeature = features[Math.floor(Math.random() * features.length)];
+  const randomFeature = sortedFeatures[
+    Math.floor(Math.random() * sortedFeatures.length)
+  ];
 
   return (
     <div className="card bg-yellow-100 disable-scrollbars">
